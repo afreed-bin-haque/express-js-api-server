d553f1eb2df84454146940cae03074b316f402ad
Afreed Bin Haque <cuatro.dev@gmail.com>
2022-07-25 17:20:05 +0600

    Data updated

--- node-express-api-server/index.js
+++ node-express-api-server/index.js
@@ -2,55 +2,8 @@ require('dotenv').config();
 require('express-group-routes');
 const express = require('express');
 const app = express();
-const manufacturer_data = require('./data/manufacturers');
-/**Manufacturer list */
-const manufacturers = manufacturer_data;
-
-/** Product List*/
-const products =[
-  {
-    serial:0001,
-    category: 'Phone',
-    product_name: 'Iphone 7',
-    manufacturer: 'Apple',
-  },
-  {
-    serial: 0002,
-    category: 'Phone',
-    product_name: 'Iphone 8',
-    manufacturer: 'Apple',
-  },
-  {
-    serial: 0003,
-    category: 'Phone',
-    product_name: 'Iphone 10',
-    manufacturer: 'Apple',
-  },
-  {
-    serial: 0004,
-    category: 'Phone',
-    product_name: 'Iphone 11',
-    manufacturer: 'Apple',
-  },
-  {
-    serial: 0005,
-    category: 'Phone',
-    product_name: 'Pixel 1',
-    manufacturer: 'Google',
-  },
-  {
-    serial: 0006,
-    category: 'Phone',
-    product_name: 'Pixel 2',
-    manufacturer: 'Google',
-  },
-  {
-    serial: 0005,
-    category: 'Phone',
-    product_name: 'Pixel 1',
-    manufacturer: 'Google',
-  },
-];
+const manufacturer_data = require('./data/manufacturers.json');
+const product_list_data = require('./data/products.json');
 
 app.get('/',function(req, res){
   res.json({ 
@@ -72,11 +25,17 @@ app.group('/', function (router) {
 app.group('/api/v1', function (router){
 
   router.get('/manufacturers', function (req, res){
-    console.log(manufacturers);
-    /* res.json({
+    res.json({
       status: 'Accepted',
-      manufacturers: manufacturers
-    }); */
+      manufacturers: manufacturer_data
+    });
+  });
+
+  router.get('/get/product/list', function (req, res) {
+    res.json({
+      status: 'Accepted',
+      product_list: product_list_data
+    });
   });
 
   router.get('/get/product/:serial/:manufacturer',function(req, res){
